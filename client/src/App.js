4825f64c044ca8c59b9348077dd0eeb3a6292bb4
Harsh97x <Rynzie@protonmail.com>
2023-02-21 15:53:12 +0530

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,18 @@ import Home from "./components/Home";
 import Resume from "./components/Resume";
 
 const App = () => {
-	const [result, setResult] = useState({});
+  const [result, setResult] = useState({});
 
-	return (
-		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<Home setResult={setResult} />} />
-					<Route path='/resume' element={<Resume result={result} />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home setResult={setResult} />} />
+          <Route path="/resume" element={<Resume result={result} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
